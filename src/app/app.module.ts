3d39a40b9c9636af9043c2d48802079243d41b7d
Oussema Dhraief <oussema.dhraief.17@gmail.com>
2023-07-14 01:04:02 +0100

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/index/dashboard.component';
+import { HistoryComponent } from './dashboard/history/history.component';
+import { DashboardModule } from './dashboard/index/dashboard.module';
+import { HistoryModule } from './dashboard/history/history.module';
+import { MapComponent } from './dashboard/map/map.component';
+import { AlertComponent } from './dashboard/alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HistoryComponent,
+    MapComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule,
+    HistoryModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#22346c",
+        secondary: "#95dd57",
+      }
+    },
+  },
+  plugins: [],
+}
+
